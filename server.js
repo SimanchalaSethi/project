e0931f16624e9006c38160640c453bcfa5358949
simanchala <simanchala.js@gmail.com>
2025-05-01 02:29:40 +0530

    service3

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 app.get('/',(req, res)=>{
     res.send('Hello World.')
 })
+//service3
+app.get("/service3", (req, res)=>{
+    res.send("This is from service3").status(200);
+})
 const port = 5001;
 app.listen(port,()=>{
 console.log("server running port number 5001")
